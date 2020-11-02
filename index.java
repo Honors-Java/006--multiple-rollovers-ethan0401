b863eeec48f352e6cdbf4bd2f3ad87c4759f4ab6
ethan0401 <71141660+ethan0401@users.noreply.github.com>
2020-11-02 23:57:19 +0000

    i made it so that the 4 quadrants will light up black when the mouse is inside of it.

--- index.java
+++ index.java
@@ -7,11 +7,25 @@ void draw() {
 	stroke(0);
 
   //start creating the lines
-
-
+ line(200,0,200,400);
+ line(0,200,400,200);
   //Fill a black color
-  
-
+  if (mouseX > width/2 && mouseY > height/2){
+    fill(0);
+    rect(width/2,height/2,width/2,height/2);
+   }
+   if (mouseX < width/2 && mouseY > height/2){
+     fill(0);
+     rect(0,height/2,width/2,height/2);
+   }
+   if (mouseX > width/2 && mouseY < height/2){
+     fill(0);
+     rect(width/2,0,width/2,height/2);
+   }
+   if (mouseX < width/2 && mouseY < height/2){
+     fill(0);
+     rect(1,1,200,200);
+   }
   //Build your conditional statement 
 	
 
